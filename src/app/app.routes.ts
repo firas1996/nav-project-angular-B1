f4fca40332b0504c12f5a1dc14b770cc1a9f2dfe
firas1996 <firas.hm.1996@gmail.com>
2025-11-18 10:22:13 +0100

    aa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+  {
+    path: 'profile',
+    component: Profile,
+  },
+];
