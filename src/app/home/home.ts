dab744b357ba8b80c34788b415e21aa31ce26ba9
firas1996 <firas.hm.1996@gmail.com>
2025-11-25 11:31:42 +0100

    aa

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+type User = {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+};
 
 @Component({
   selector: 'app-home',
@@ -7,4 +15,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  users = signal<User[] | undefined>([]);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscribe = this.httpClient.get('http://localhost:1234/user').subscribe({
+      next: (res: any) => {
+        this.users.set(res.data);
+        console.log(this.users());
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscribe.unsubscribe();
+    });
+  }
+}
